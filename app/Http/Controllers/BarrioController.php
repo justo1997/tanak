f8444ceba2991f7ab2465f78cbb3f19926001acb
LuisSamaniego27 <luisamaniegobusiness@gmail.com>
2022-04-14 17:09:33 -0400

    nuevo cambio

--- app/Http/Controllers/BarrioController.php
+++ app/Http/Controllers/BarrioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Barrio;
+use App\Models\Ciudad;
 
 use Illuminate\Http\Request;
 
@@ -13,5 +14,63 @@ class BarrioController extends Controller
 
         return view('barrio.index')->with('barrios', $barrios);
     }
+
+    public function create()
+    {
+        $ciudades = Ciudad::all();
+
+        return view('barrio.crear', compact('ciudades'));
+    }
+
+    public function store(Request $request)
+    {
+        $barrios = new Barrio();
+        $barrios->nombre = $request->get('nombre');
+        $barrios->ciudades_id = $request->get('ciudad');
+        $barrios->save();
+        session()->flash('message', 'Registro Exitoso');
+        return redirect ('/barrios');
+        
+    }
+
+   
+    public function show($id)
+    {
+        //
+    }
+
+    
+    public function edit($id)
+    {
+
+        $ciudades = Ciudad::all();
+        
+
+        $barrio = Barrio::find($id);
+        
+
+        return view('barrio.editar', compact('ciudades'), compact('barrio'));
+    }
+
+   
+    public function update(Request $request, $id)
+    {
+        $barrio = Barrio::find($id);
+        $barrio->nombre = $request->get('nombre');
+        $barrio->save();
+        session()->flash('message', 'Actualización exitosa');
+        return redirect ('/barrios');
+    }
+
+  
+    public function destroy($id)
+    {
+        $barrio = Barrio::find($id);
+        $barrio->delete();
+        session()->flash('message', 'Registro eliminado correctamente');
+        return redirect('/barrios');
+
+    }
+    
 
 }
--- app/Models/Barrio.php
+++ app/Models/Barrio.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Barrio extends Model
 {
     protected $table = 'GEO_BARRIOS';
+
+    public function ciudades(){
+        return $this->belongsTo('App\Ciudad', 'ID_CIUDAD');
+    }
 }
